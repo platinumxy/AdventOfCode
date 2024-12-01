0cade78c9bb5f9fa451814ee4a7283e3af083a07
platinumxy <83834735+platinumxy@users.noreply.github.com>
2024-12-01 15:01:51 +0000

    Day 1
    Didn't do in OCaml as laptop was having issues

---
+++ AoC24/lib/D1.py
@@ -0,0 +1,19 @@
+def read_input() -> list[str]:
+    with open('./files/day1.txt') as f:
+        return f.read().splitlines()
+
+def split_line(line: str) ->  tuple[int, int]:
+    left, right = line.split("   ")
+    return tuple(map(int, (left, right)))
+
+lefts, rights = zip(*[split_line(line) for line in read_input()])
+lefts, rights = list(lefts), list(rights)
+
+lefts.sort()
+rights.sort()
+total_distance = sum(map(lambda t: abs(t[0] - t[1]), zip(lefts, rights)))
+
+similarity_score = sum(map(lambda l: rights.count(l) * l, lefts))
+    
+print(f"=== Day One ===\nPart One: {total_distance}")  
+print(f"Part Two: {similarity_score}")
