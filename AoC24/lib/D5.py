5a81a0791264bfe3f0d29c03410e749df3bf07de
Archie Spiller <83834735+platinumxy@users.noreply.github.com>
2024-12-05 10:55:14 +0000

    Day 5: A deceptively simple challenge

---
+++ AoC24/lib/D5.py
@@ -0,0 +1,47 @@
+def parse_input() -> tuple[list[tuple[str, str]], list[list[str]]]:
+    with open('./files/day5.txt') as f:
+        lines = f.read().splitlines()
+        split = lines.index("")
+        lhs = [tuple(line.split("|")) for line in lines[:split]]
+        rhs = [line.split(",") for line in lines[split+1:]]
+        return lhs, rhs
+
+def gen_rules(rs: list[tuple[str, str]]) -> dict[str, dict[str, list[str]]]:
+    d = {k: {"smaller": [], "larger": []} for pair in rs for k in pair}
+    for sm, lg in rs:
+        d[sm]["larger"].append(lg)
+        d[lg]["smaller"].append(sm)
+    return d
+
+def valid_update(rules: dict[str, dict[str, list[str]]], update: list[str]) -> bool:
+    return all(
+        not ((check in rules[val]["smaller"] and j > i) or (check in rules[val]["larger"] and j < i))
+        for i, val in enumerate(update)
+        for j, check in enumerate(update)
+    )
+        
+def gen_valid_updates() -> list[str]:
+    global RULES, UPDATES
+    return [update for update in UPDATES if valid_update(RULES, update)]
+
+def gen_invalid_updates() -> list[str]:
+    global RULES, UPDATES
+    return [fix_invalid(RULES, update) for update in UPDATES if not valid_update(RULES, update)]
+
+def fix_invalid(rules: dict[str, dict[str,list[str]]], l:list[str]) -> list[str]:
+    out = []
+    for val in l:
+        for i, check in enumerate(out):
+            if check in rules[val]["larger"]:
+                out.insert(i, val)
+                break
+        else:
+            out.append(val)
+    return out
+
+def calc_val(l: list[any]) -> int:
+    return sum(map(lambda l: int(l[len(l)//2]), l))
+
+__rules, UPDATES = parse_input()
+RULES = gen_rules(__rules)
+print("=== Day 5: ===\nPart One:", calc_val(gen_valid_updates()), "\nPart Two:", calc_val(gen_invalid_updates())) 
